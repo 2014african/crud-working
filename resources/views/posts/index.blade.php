c09b19afc4d684754ed446166e7e1f98e6fb0b2b
zirodev <zirodev23@zirodev23.com>
2024-11-05 23:18:06 +0200

    first commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+    <h1>All posts</h1>
+    <a href="/posts/create">Create post</a>
+    <ul>
+        @foreach($posts as $post)
+            <li>
+                <h2>Title: {{ $post->title }}</h2>
+                <p>Content: {{ $post->content }}</p>
+                <div>
+                    <a href="/posts/{{ $post->id }}">Show</a>
+                    <a href="/posts/{{ $post->id }}/edit">Edit</a>
+                    <form action="/posts/{{ $post->id }}/destroy" method="post">
+                        @csrf
+                        @method('delete')
+                        <input type="submit" value="Delete">
+                    </form>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+</x-app-layout>
