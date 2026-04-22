7edeffcc2ad271fc67beef826237879f9598d06c
adrians <polisadrians>
2026-04-22 12:56:07 +0300

    added dropdown menu in show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -17,9 +17,15 @@
         @csrf
         @method('PUT')
 
-        <label for="status">Status</label>
-        <input type="text" id="status" name="status" value="{{ old('status', $post->status) }}" placeholder="Enter status">
+         Status: <select name="subject" id="subject">
+            <option value="draft" selected="selected">Draft</option>
+            <option value="publish" selected="selected">Published</option>
+            <option value="archive" selected="selected">Archived</option>
+            </select>
 
-        <input type="submit" value="Update status">
+        <!-- <label for="status">Status</label>
+        <input type="text" id="status" name="status" value="{{ old('status', $post->status) }}" placeholder="Enter status"> -->
+
+        <!-- <input type="submit" value="Update status"> -->
     </form>
 </x-app-layout>
